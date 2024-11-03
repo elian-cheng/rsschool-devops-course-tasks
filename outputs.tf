0d70ba680ef4e5088d1e9f0cfc91b35598233341
Olga Chernega <eliang.cheng@gmail.com>
2024-11-03 18:19:38 +0200

    fix: remove bastion host reference

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,6 @@ output "private_subnets" {
   value       = aws_subnet.K8S_private_subnet[*].id
 }
 
-output "bastion_host_public_ip" {
-  description = "The public IP of the bastion host"
-  value       = aws_instance.K8S_bastion_host.public_ip
-}
-
 output "K8s_master_private_ip" {
   description = "The private IP of the private instance K8s master"
   value       = aws_instance.K8S_K3S_master.private_ip
